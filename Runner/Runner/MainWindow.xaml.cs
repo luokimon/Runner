a62583bb43f94aac6244dafb38b4782b26828132
kimon <luokimon@163.com>
2016-11-22 14:46:40 +0800

    Add DataGrid Code

--- Runner/Runner/MainWindow.xaml.cs
+++ Runner/Runner/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ using LibUsbDotNet.Info;
 using LibUsbDotNet.DeviceNotify;
 using System.Collections.ObjectModel;
 using System.Windows.Controls.Primitives;
+using System.Data;
+
 namespace Runner
 {
     /// <summary>
@@ -53,15 +55,174 @@ namespace Runner
             return;
         }
 
-        //UsbDetector usbDetector;
 
+        // Put the next line into the Declarations section.
+        private DataSet _dataSet;
+        private void MakeDataTables()
+        {
+            // Run all of the functions
+            MakeParentTable();
+            MakeChildTable();
+            MakeDataRelation();
+            BindToDataGrid();
+        }
+
+        private void MakeParentTable()
+        {
+            // Create a new DataTable.
+            DataTable table = new DataTable("ParentTable");
+            // Declare variables for DataColumn and DataRow objects.
+            DataColumn column;
+            DataRow row;
+
+            // Create new DataColumn, set DataType,
+            // ColumnName and add to DataTable.
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32");
+            column.ColumnName = "id";
+            column.ReadOnly = true;
+            column.Unique = true;
+            // Add the Column to the DataColumnCollection.
+            table.Columns.Add(column);
+
+            // Create second column
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "ParentItem";
+            column.AutoIncrement = false;
+            column.Caption = "ParentItem";
+            column.ReadOnly = false;
+            column.Unique = false;
+            // Add the column to the table.
+            table.Columns.Add(column);
+
+            // Make the ID column the primary key column.
+            DataColumn[] PrimaryKeyColumns = new DataColumn[1];
+            PrimaryKeyColumns[0] = table.Columns["id"];
+            table.PrimaryKey = PrimaryKeyColumns;
+
+            // Instantiate the DataSet variable.
+            _dataSet = new DataSet();
+            // Add the new DataTable to the DataSet.
+            _dataSet.Tables.Add(table);
+
+            // Create three new DataRow objects and add
+            // them to the DataTable
+            for(int i=0; i<=2;i++)
+            {
+                row = table.NewRow();
+                row["id"] = i;
+                row["ParentItem"] = "ParentItem " + i;
+                table.Rows.Add(row);
+            }
+
+        }
+
+        private void MakeChildTable()
+        {
+            // Create a new DataTable.
+            DataTable table = new DataTable("ChildTable");
+            DataColumn column;
+            DataRow row;
+
+            // Create first column and add to the DataTable.
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32");
+            column.ColumnName = "ChildID";
+            column.AutoIncrement = true;
+            column.Caption = "ID";
+            column.ReadOnly = true;
+            column.Unique = true;
+
+            // Add the column to the DataColumnCollection.
+            table.Columns.Add(column);
+
+            // Create second column.
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.String");
+            column.ColumnName = "ChildItem";
+            column.AutoIncrement = false;
+            column.Caption = "ChildItem";
+            column.ReadOnly = false;
+            column.Unique = false;
+            table.Columns.Add(column);
+
+            // Create third column.
+            column = new DataColumn();
+            column.DataType = System.Type.GetType("System.Int32");
+            column.ColumnName = "ParentID";
+            column.AutoIncrement = false;
+            column.Caption = "ParentID";
+            column.ReadOnly = false;
+            column.Unique = false;
+            table.Columns.Add(column);
+
+            _dataSet.Tables.Add(table);
+
+            // Create three sets of DataRow objects,  
+            // five rows each, and add to DataTable. 
+            for (int i = 0; i <= 4; i++)
+            {
+                row = table.NewRow();
+                row["childID"] = i;
+                row["ChildItem"] = "Item " + i;
+                row["ParentID"] = 0;
+                table.Rows.Add(row);
+            }
+            for (int i = 0; i <= 4; i++)
+            {
+                row = table.NewRow();
+                row["childID"] = i + 5;
+                row["ChildItem"] = "Item " + i;
+                row["ParentID"] = 1;
+                table.Rows.Add(row);
+            }
+            for (int i = 0; i <= 4; i++)
+            {
+                row = table.NewRow();
+                row["childID"] = i + 10;
+                row["ChildItem"] = "Item " + i;
+                row["ParentID"] = 2;
+                table.Rows.Add(row);
+            }
+
+        }
+
+        private void MakeDataRelation()
+        {
+            // DataRelation requires two DataColumn  
+            // (parent and child) and a name.
+            DataColumn parentColumn =
+                _dataSet.Tables["ParentTable"].Columns["id"];
+            DataColumn childColumn =
+                _dataSet.Tables["ChildTable"].Columns["ParentID"];
+            DataRelation relation = new
+                DataRelation("parent2Child", parentColumn, childColumn);
+            _dataSet.Tables["ChildTable"].ParentRelations.Add(relation);
+        }
+
+        private void BindToDataGrid()
+        {
+            // Instruct the DataGrid to bind to the DataSet, with the  
+            // ParentTable as the topmost DataTable.
+            //dataGrid1.SetDataBinding(_dataSet, "ParentTable");
+            dataGrid1.ItemsSource = _dataSet.Tables[1].DefaultView;
+        }
+
+        //UsbDetector usbDetector;
         public MainWindow()
         {
+
+            InitializeComponent();
+
+            MakeDataTables();
+
             _mainDirPath = System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
 
             _fileSetting = _mainDirPath+ "\\ToucherSetting.xml";
 
-            InitializeComponent();
+            
+
 
 #if false
             usbDetector = new UsbDetector();
